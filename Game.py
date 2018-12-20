6ca14873885fce354cbfb3fdf91f37d12292fc9f
Aleksei Bulygin <eksuby@rambler.ru>
2018-12-20 16:44:27 +0300

    create "create_missile" func change "our_" and "enemy_missile"

--- Game.py
+++ Game.py
@@ -8,7 +8,72 @@ window.bgpic('images/background.png')
 
 BASE_X, BASE_Y = 0, -230
 
-def foo(missiles):
+
+def calc_lenght(x, y, x1, y1):
+    dx = x1 - x
+    dy = y1 - y
+    lenght = (dx ** 2 + dy ** 2) ** 0.5
+    cos_alpha = dx / lenght
+    alpha = math.acos(cos_alpha)
+    alpha = math.degrees(alpha)
+    if dy < 0:
+        alpha = -alpha
+    return alpha
+
+
+def create_missile(side, x, y, x1, y1):
+    missile = turtle.Turtle(visible=False)
+    if side == "our":
+        missile.color('white')
+    elif side == "enemy":
+        missile.color('red')
+    missile.penup()
+    missile.setpos(x, y)
+    missile.pendown()
+    lenght = missile.towards(x1, y1)
+    missile.setheading(lenght)
+    missile.showturtle()
+    info = {"missile": missile,
+            "target": [x1, y1],
+            "status": "launched"}
+    missiles.append(info)
+
+
+def enemy_missile():
+    start_x = randint(-400, 400)
+    start_y = 300
+    # missile = turtle.Turtle(visible=False)
+    # missile.color('red')
+    # missile.penup()
+    # missile.setpos(start_x, start_y)
+    # missile.pendown()
+    # lenght = missile.towards(x=BASE_X, y=BASE_Y)
+    # missile.setheading(lenght)
+    # missile.showturtle()
+    # info = {"missile": missile,
+    #         "target": [BASE_X, BASE_Y],
+    #         "status": "launched"}
+    # missiles.append(info)
+    create_missile("enemy", start_x, start_y, BASE_X, BASE_Y)
+
+
+def our_missile(x, y):
+    # missile = turtle.Turtle(visible=False)
+    # missile.color('white')
+    # missile.penup()
+    # missile.setpos(BASE_X, BASE_Y)
+    # missile.pendown()
+    # lenght = calc_lenght(BASE_X, BASE_Y, x, y)
+    # missile.setheading(lenght)
+    # missile.showturtle()
+    # info = {"missile": missile,
+    #         "target": [x, y],
+    #         "status": "launched"}
+    # missiles.append(info)
+    create_missile("our", BASE_X, BASE_Y, x, y)
+
+
+def launch(missiles):
     for info in missiles:
         missile = info["missile"]
         status = info["status"]
@@ -26,47 +91,6 @@ def foo(missiles):
     dead_missiles = [i for i in missiles if i["status"] == "dead"]
     for dead in dead_missiles:
         missiles.remove(dead)
-
-def calc_lenght(x, y, x1, y1):
-    dx = x1 - x
-    dy = y1 - y
-    lenght = (dx ** 2 + dy ** 2) ** 0.5
-    cos_alpha = dx / lenght
-    alpha = math.acos(cos_alpha)
-    alpha = math.degrees(alpha)
-    if dy < 0:
-        alpha = -alpha
-    return alpha
-
-def enemy_missile():
-    start_x = randint(-400, 400)
-    start_y = 300
-    missile = turtle.Turtle(visible=False)
-    missile.color('red')
-    missile.penup()
-    missile.setpos(start_x, start_y)
-    missile.pendown()
-    lenght = calc_lenght(start_x, start_y, BASE_X, BASE_Y)
-    missile.setheading(lenght)
-    missile.showturtle()
-    info = {"missile": missile,
-            "target": [BASE_X, BASE_Y],
-            "status": "launched"}
-    missiles.append(info)
-
-def our_missile(x, y):
-    missile = turtle.Turtle(visible=False)
-    missile.color('white')
-    missile.penup()
-    missile.setpos(BASE_X, BASE_Y)
-    missile.pendown()
-    lenght = calc_lenght(BASE_X, BASE_Y, x, y)
-    missile.setheading(lenght)
-    missile.showturtle()
-    info = {"missile": missile,
-            "target": [x, y],
-            "status": "launched"}
-    missiles.append(info)
 
 
 window.onclick(our_missile)
@@ -95,4 +119,4 @@ while True:
     on_fire = randint(1, 100)
     if on_fire == 5:
         enemy_missile()
-    foo(missiles)
+    launch(missiles)
